17e3226a860652b4371a7eefc28e47c80a348e76
arjanmalda <arjanmalda@gmail.com>
2022-05-04 07:47:55 +0200

    Step 2: data made available through API and fetched in front-end

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,189 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import logo from "./logo.svg";
 import "./App.css";
+import Table from "./components/Table";
 
+interface Webcams {
+  Camera: string;
+  location: string;
+  latitude: string;
+  longitude: string;
+  id: number;
+}
 function App() {
-  return <div className="App">Webcams in Utrecht</div>;
+  const [webcamData, setWebcamData] = useState<Webcams[]>();
+  const [cameras3, setCameras3] = useState<Webcams[]>();
+  const [cameras5, setCameras5] = useState<Webcams[]>();
+  const [cameras35, setCameras35] = useState<Webcams[]>();
+  const [camerasOther, setCamerasOther] = useState<Webcams[]>();
+
+  const createTableColumns = (webcams: Webcams[]) => {
+    let row1 = [];
+    let row2 = [];
+    let row3 = [];
+    let row4 = [];
+
+    for (let i = 0; i < webcams.length; i++) {
+      if (parseInt(webcams[i].Camera.replace(/\D/g, "")) % 3 === 0) {
+        row1.push(webcams[i]);
+      }
+      if (parseInt(webcams[i].Camera.replace(/\D/g, "")) % 5 === 0) {
+        row2.push(webcams[i]);
+      }
+      if (
+        parseInt(webcams[i].Camera.replace(/\D/g, "")) % 3 === 0 &&
+        parseInt(webcams[i].Camera.replace(/\D/g, "")) % 5 === 0
+      ) {
+        row3.push(webcams[i]);
+      }
+      if (
+        parseInt(webcams[i].Camera.replace(/\D/g, "")) % 3 !== 0 &&
+        parseInt(webcams[i].Camera.replace(/\D/g, "")) % 5 !== 0
+      ) {
+        row4.push(webcams[i]);
+      }
+      setCameras3(row1);
+      setCameras5(row2);
+      setCameras35(row3);
+      setCamerasOther(row4);
+    }
+    setWebcamData(webcams);
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:3002")
+      .then((response) => response.json())
+      .then((data) => createTableColumns(data));
+  }, [webcamData]);
+
+  if (!webcamData) {
+    return <div className="App">Webcams are loading</div>;
+  } else {
+    return (
+      <div className="App">
+        <table id="cameraTableContainer">
+          <tbody>
+            <tr>
+              <td>
+                <table id="column3">
+                  <thead>
+                    <tr>
+                      <th>Cameras 3</th>
+                    </tr>
+                    <tr>
+                      <th>Number</th>
+                      <th>Name</th>
+                      <th>Latitude</th>
+                      <th>Longitude</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {cameras3 &&
+                      cameras3.map((webcam) => {
+                        return (
+                          <tr>
+                            <td>{webcam.Camera}</td>
+                            <td>{webcam.location}</td>
+                            <td>{webcam.latitude}</td>
+                            <td>{webcam.longitude}</td>
+                          </tr>
+                        );
+                      })}
+                  </tbody>
+                  ;
+                </table>
+              </td>
+              <td>
+                <table id="column5">
+                  <thead>
+                    <tr>
+                      <th>Cameras 5</th>
+                    </tr>
+                    <tr>
+                      <th>Number</th>
+                      <th>Name</th>
+                      <th>Latitude</th>
+                      <th>Longitude</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {cameras5 &&
+                      cameras5.map((webcam) => {
+                        return (
+                          <tr>
+                            <td>{webcam.Camera}</td>
+                            <td>{webcam.location}</td>
+                            <td>{webcam.latitude}</td>
+                            <td>{webcam.longitude}</td>
+                          </tr>
+                        );
+                      })}
+                  </tbody>
+                </table>
+              </td>
+              <td>
+                <table id="column15">
+                  <thead>
+                    <tr>
+                      <th>Cameras 3 &amp; 5</th>
+                    </tr>
+                    <tr>
+                      <th>Number</th>
+                      <th>Name</th>
+                      <th>Latitude</th>
+                      <th>Longitude</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {cameras35 &&
+                      cameras35.map((webcam) => {
+                        return (
+                          <tr>
+                            <td>{webcam.Camera}</td>
+                            <td>{webcam.location}</td>
+                            <td>{webcam.latitude}</td>
+                            <td>{webcam.longitude}</td>
+                          </tr>
+                        );
+                      })}
+                  </tbody>
+                </table>
+              </td>
+              <td>
+                <table id="columnOther">
+                  <thead>
+                    <tr>
+                      <th>Cameras Overig</th>
+                    </tr>
+                    <tr>
+                      <th>Number</th>
+                      <th>Name</th>
+                      <th>Latitude</th>
+                      <th>Longitude</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {camerasOther &&
+                      camerasOther.map((webcam) => {
+                        return (
+                          <tr>
+                            <td>{webcam.Camera}</td>
+                            <td>{webcam.location}</td>
+                            <td>{webcam.latitude}</td>
+                            <td>{webcam.longitude}</td>
+                          </tr>
+                        );
+                      })}
+                  </tbody>
+                </table>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
--- server/index.ts
+++ server/index.ts
@@ -1,33 +1,35 @@
 import express from "express";
+
 import csv from "csvtojson";
 
 interface Webcams {
-  camera: string;
+  Camera: string;
   location: string;
   latitude: string;
   longitude: string;
+  id: number | undefined;
 }
 
-// Import csv data
+// Import csv data and parse csv data to object
 const csvFilePath = "./cameras-defb.csv";
+let webcams: Array<Webcams>;
+csv()
+  .fromFile(csvFilePath)
+  .then((jsonObject) => {
+    webcams = jsonObject;
 
-//   Read user input from command line
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+    //   Read user input from command line
+    const readline = require("readline").createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-// parse csv data to object and print search result to console
+    //  print search result to console
 
-readline.question(`Look for a webcam: `, (searchTerm: string) => {
-  let webcams: Array<Webcams>;
-  csv()
-    .fromFile(csvFilePath)
-    .then((jsonObject) => {
-      webcams = jsonObject;
-
+    readline.question(`Look for a webcam: `, (searchTerm: string) => {
       let searchResult = [];
       for (let i = 0; i < webcams.length; i++) {
+        webcams[i].id = i;
         if (
           webcams[i].location
             .toLowerCase()
@@ -39,18 +41,45 @@ readline.question(`Look for a webcam: `, (searchTerm: string) => {
       searchResult.length > 0
         ? console.log("Search result: ", searchResult)
         : console.log("No results");
-    });
+      app.get("/", (req, res) => {
+        res.send(webcams);
+      });
 
-  readline.close();
-});
+      readline.close();
+    });
+  });
 
 const app = express();
 const port = 3002;
 
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  // Pass to next layer of middleware
+  next();
+});
+
 app.get("/", (req, res) => {
-  res.send(`Look for a webcam`);
+  res.send(webcams);
 });
 
 app.listen(port, () => {
-  //   console.log(`The application is listening on port ${port}!`);
+  console.log(`Listening on port ${port}!`);
 });
